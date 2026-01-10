555deeb85876bcdc02523052b157c53624a6a0da
priyankaflexadvertsing-rgb <priyankaflexadvertsing@gmail.com>
2026-01-10 21:07:49 +0530

    Update uril.jsx

--- shop_frontend/uri/uril.jsx
+++ shop_frontend/uri/uril.jsx
@@ -1,5 +1,6 @@
-export const SERVER_URI = "http://localhost:8000/api/v1"
-export const SOCKET_URI = "http://localhost:8000"
+// export const SERVER_URI = "http://localhost:8000/api/v1"
+// export const SOCKET_URI = "http://localhost:8000"
 
-// export const SERVER_URI = " https://alankar-shop-mangment-project.onrender.com/api/v1"
-// export const SOCKET_URI = " https://alankar-shop-mangment-project.onrender.com"
+export const SERVER_URI = " https://alankar-shop-mangment-project.onrender.com/api/v1"
+
+export const SOCKET_URI = " https://alankar-shop-mangment-project.onrender.com"
